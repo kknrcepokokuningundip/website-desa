3f9a3e9ca87eb54ce25c96baef53134f8fafa6d9
yuniarctra <yuniarcitra12@gmail.com>
2026-07-22 13:50:09 +0700

    Update home page

--- js/script.js
+++ js/script.js
@@ -10,3 +10,9 @@ window.addEventListener("scroll", function(){
 
 });
 
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
